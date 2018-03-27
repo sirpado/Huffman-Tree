47fca2cf1e82f1762752c677f5fb5469c0f8f2e5
Omer Sirpad <sirpado61@gmail.com>
2018-03-27 14:55:43 -0400

    Encoding is working

--- HufmannEncoderDecoder.java
+++ HufmannEncoderDecoder.java
@@ -1,11 +1,16 @@
 package assign1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.math.BigInteger;
+import java.util.HashMap;
 
 /**
  * Assignment 1
@@ -20,7 +25,7 @@ import base.compressor;
 
 public class HufmannEncoderDecoder implements compressor
 {
-
+	
 	public HufmannEncoderDecoder()
 	{
 		// TODO Auto-generated constructor stub
@@ -28,6 +33,7 @@ public class HufmannEncoderDecoder implements compressor
 	@Override
 	public void Compress(String[] input_names, String[] output_names)
 	{
+		
 		File file = new File(input_names[0]);
 
 		BufferedReader br = null;
@@ -38,50 +44,71 @@ public class HufmannEncoderDecoder implements compressor
 			e.printStackTrace();
 		}
 
+		Writer writer = null;
 		String org_text = null;
-		String byte_val;
+		String text = null;
+		
 		int freq[] = new int [256];
 		try {
 			while ((org_text = br.readLine()) != null)
 			{
-				for (int i = 0; i < org_text.length();i += 8)
-				{
-					byte_val = org_text.substring(i, i+8);
-					int ch = Integer.parseInt(byte_val, 2);
-					freq[ch]++;
-				}
-				
+				org_text = org_text.replaceAll("\\s+", "");
+				if (text == null)
+					text = org_text;
+				else
+					text = text + org_text;
 			}
-		} catch (IOException e)
-		{
-			e.printStackTrace();
-		}
-	
+			byte [] arr = new BigInteger(text,2).toByteArray();
+			for (int i = 0; i < arr.length; i++)
+			{
+				freq[(arr[i])] ++;
+			}
+			
+			HuffmanNode root = HuffmanNode.BuildTree(freq);
+			
+			HashMap <Character,String > dictionery = new HashMap<Character,String>();
+			root.buildDictionery(dictionery,"");
+			
+			try 
+			{
+			    writer = new BufferedWriter(new OutputStreamWriter(
+			          new FileOutputStream(output_names[1]), "utf-8"));
+			    for (int j = 0; j < arr.length; j++)
+			    {
+			    	char c = (char)arr[j];
+			    	String s = dictionery.get(c);
+			    	writer.write(s);
+			    }
+			} 
+			catch (IOException e) {  e.printStackTrace();}
+			finally
+			{
+			   try
+			   {writer.close();} catch (Exception e) {e.printStackTrace();}
+			}
+		} 
+		catch (IOException e) {e.printStackTrace();}
+	}
 
+	@Override
+	public void Decompress(String[] input_names, String[] output_names)
+	{
+		// TODO Auto-generated method stub
 
-	HuffmanNode root = new HuffmanNode(0, '`');
-	root = root.BuildTree(freq);
-}
+	}
 
-@Override
-public void Decompress(String[] input_names, String[] output_names)
-{
-	// TODO Auto-generated method stub
+	@Override
+	public byte[] CompressWithArray(String[] input_names, String[] output_names)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 
-}
-
-@Override
-public byte[] CompressWithArray(String[] input_names, String[] output_names)
-{
-	// TODO Auto-generated method stub
-	return null;
-}
-
-@Override
-public byte[] DecompressWithArray(String[] input_names, String[] output_names)
-{
-	// TODO Auto-generated method stub
-	return null;
-}
+	@Override
+	public byte[] DecompressWithArray(String[] input_names, String[] output_names)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
